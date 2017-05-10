5fd5ecee93cbd6702bd70ceddbaf5b796fd0972b
BuildTools <wuppinana@web.de>
2017-05-10 15:24:19 +0200

    App.js instanz Client und so

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var Client = require('./code/client.js');
 
 server.listen(4217);
 
@@ -11,7 +12,10 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+clients = [];
+
 io.on('connection', function (socket) {
+  clients.push(new Client(socket));
   socket.on('login',function(data){
     console.log("Player: "+data.name+" connected into lobby: "+data.lobby);
   });
