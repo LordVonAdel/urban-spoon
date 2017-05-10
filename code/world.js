fe8d00357b32a1da17ef4e9639de47773baafff7
Mario <mario.gillmann0@gmail.com>
2017-05-10 16:28:53 +0200

    save!

---
+++ code/world.js
@@ -0,0 +1,15 @@
+module.exports = function(terrain){
+  this.terrain = terrain;
+
+  this.getHeight = function(x) {
+
+  }
+
+  this.setHeight = function(xMin, xMax, height) {
+
+  }
+
+  this.addHeight = function(xMin, xMax, height) {
+      
+  }
+}
