537ba147f548292b2bb70fa508add830779ea79a
BuildTools <wuppinana@web.de>
2017-05-10 18:08:02 +0200

    add berge generator

--- code/world.js
+++ code/world.js
@@ -3,6 +3,7 @@ module.exports = function(seed, generator){
 }
 
 function Terrain(seed, generator){
+  this.length = 500;
 
   this.getHeight = function(x) {
 
@@ -12,6 +13,14 @@ function Terrain(seed, generator){
 
   }
 
+  this.nodeSet = function(index, height){
+
+  }
+
+  this.nodeAdd = function(index, height){
+    
+  }
+
   this.addHeight = function(xMin, xMax, height) {
       
   }
@@ -19,6 +28,8 @@ function Terrain(seed, generator){
   this.digTerrain = function(x, radius, power) {
 
   }
+
+  generators["berge"](this);
 }
 
 generators = {
@@ -26,6 +37,15 @@ generators = {
 
   },
   berge: function(terrain){
-    
+    var maxH = 400; //maximum height
+    var h = maxH/2;
+    var s = 0;
+    for(var i=0; i<terrain.length; i++){
+      terrain.nodeSet(i,h);
+      s+=Math.random()*8-4+(-8*((h/maxH)-0.5));
+      s=Math.min(30,Math.max(s,-30));
+      h+=s;
+      h = Math.min(maxH-30,Math.max(h,30));
+    }
   }
 }
