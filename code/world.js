ef7ae50718ee87e914a9cef1d1f234d89b9adfd9
Mario <mario.gillmann0@gmail.com>
2017-05-10 16:45:15 +0200

    terrain

--- code/world.js
+++ code/world.js
@@ -1,5 +1,24 @@
-module.exports = function(terrain){
-  this.terrain = terrain;
+module.exports = function(seed){
+  this.terrain = new Terrain();
+
+  this.getHeight = function(x) {
+    this.terrain.getHeight(x);
+  }
+
+  this.setHeight = function(xMin, xMax, height) {
+    this.terrain.setHeight(xMin, xMax, height);
+  }
+
+  this.addHeight = function(xMin, xMax, height) {
+    this.terrain.addHeight(xMin, xMax, height);
+  }
+
+  this.digTerrain = function(x, radius, power) {
+    this.terrain.digTerrain(x, radius, power);
+  }
+}
+
+function Terrain(seed){
 
   this.getHeight = function(x) {
 
