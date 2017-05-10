ae7833fbf804ec80d68b251e08631ead3c2f991a
Mario <mario.gillmann0@gmail.com>
2017-05-10 16:46:32 +0200

    ALLES WIEDER LÖSCHEN!

--- code/world.js
+++ code/world.js
@@ -1,21 +1,5 @@
 module.exports = function(seed){
-  this.terrain = new Terrain();
-
-  this.getHeight = function(x) {
-    this.terrain.getHeight(x);
-  }
-
-  this.setHeight = function(xMin, xMax, height) {
-    this.terrain.setHeight(xMin, xMax, height);
-  }
-
-  this.addHeight = function(xMin, xMax, height) {
-    this.terrain.addHeight(xMin, xMax, height);
-  }
-
-  this.digTerrain = function(x, radius, power) {
-    this.terrain.digTerrain(x, radius, power);
-  }
+  this.terrain = new Terrain(seed);
 }
 
 function Terrain(seed){
